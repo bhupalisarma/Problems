05d91e6e4a94358e818fc84936d3b7da9ef0f982
Bhupali Sarma <85697658+bhupalisarma@users.noreply.github.com>
2022-11-05 15:42:20 +0530

    Time: 6 ms (85.95%), Space: 7.4 MB (78.17%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+bool isPalindrome(string s) {
+    int st=0;
+    int end=s.length()-1;
+    while(st<=end){
+        if(!isalnum(s[st]))
+        {
+            st++; 
+            continue;              //isalnum to check whether its alphabet or number
+        } 
+        if(!isalnum(s[end]))
+        {
+            end--; 
+            continue;
+        }
+        if(tolower(s[st])!=tolower(s[end])){ 
+            return false;           //tolower to cover it into lower case incase its case sensitive
+        }
+        else{
+            st++;
+            end--;
+        }   
+      
+    }
+       return true;
+}
+};
