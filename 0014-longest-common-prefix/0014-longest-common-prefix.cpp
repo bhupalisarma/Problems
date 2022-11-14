e450dfef61dca35a67d872c9e40bc3f990347c3d
Bhupali Sarma <85697658+bhupalisarma@users.noreply.github.com>
2022-11-14 20:05:55 +0530

    Time: 8 ms (61.19%), Space: 9.1 MB (95.00%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,34 @@
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+        
+//     }
+// };
+
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        
+        // Sort the vector
+        sort(strs.begin(), strs.end());
+        string ans = ""; // for storing answer
+        
+        string a = strs[0]; // after sorting first element
+        string b = strs[n-1]; // after sorting last element
+        
+        // We can compare for first and last , since they are sorted
+        // i < a.length() ==> common prefix we can select least word
+        for(int i = 0; i < a.length(); i++){
+            if(a[i] == b[i]){
+                // If same add in answer
+                ans += a[i];
+            }else{
+                break;
+            }
+        }
+        return ans;
+    }
+};
