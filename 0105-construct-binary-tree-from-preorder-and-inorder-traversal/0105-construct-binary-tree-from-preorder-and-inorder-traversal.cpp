0f3527520be237dccdbc5e1f0ae38104783e1361
Bhupali Sarma <85697658+bhupalisarma@users.noreply.github.com>
2022-12-15 01:28:31 +0530

    Time: 16 ms (95.09%), Space: 26.4 MB (29.15%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,20 +11,31 @@
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int rootIdx = 0;
-        return build(preorder, inorder, rootIdx, 0, inorder.size()-1);
+//with map
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder,int ps,int pe,int is,int ie,unordered_map<int,int>& map){
+	// 4) base conditon  
+        if(ps>pe||is>ie)return NULL;
+	// 5) create root node
+        TreeNode* root = new TreeNode(preorder[ps]);
+	// 6) find postion of root in inorder vec. using map 
+        int inroot = map[root->val];
+	// 7) find the left side len. eg:  [9,3,15,20,7];  root=3, left len = rootidx(1) - is(0)
+        int left = inroot-is;
+	// 8) call on left side and attach to root->left 
+        root->left = solve(preorder,inorder,ps+1,ps+left,is,inroot-1,map);
+	// 9) call on right side and attach to root->right	
+        root->right = solve(preorder,inorder,ps+left+1,pe,inroot+1,ie,map);
+	// 10) return root
+        return root;
     }
-    
-    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int& rootIdx, int left, int right) {
-        if (left > right) return NULL;
-        int pivot = left;  // find the root from inorder
-        while(inorder[pivot] != preorder[rootIdx]) pivot++;
-        
-        rootIdx++;
-        TreeNode* newNode = new TreeNode(inorder[pivot]);
-        newNode->left = build(preorder, inorder, rootIdx, left, pivot-1);
-        newNode->right = build(preorder, inorder, rootIdx, pivot+1, right);
-        return newNode;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+	// 1) maintain a map
+        unordered_map<int,int> map;
+        int m = preorder.size();
+        int n = inorder.size();
+   // 2) mapping in inorder to corresponding index
+        for(int i=0;i<n;i++)map[inorder[i]] = i;
+	// 3) call solve function 	
+         return solve(preorder,inorder,0,m-1,0,n-1,map);
     }
 };
